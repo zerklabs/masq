23c5bf2feede627412ea25f51da1d78e63a485a2
Robin Harper <cabrel@zerklabs.com>
2014-03-17 16:08:53 -0400

    Updated build script

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-go build -o bin/masq-daemon github.com/zerklabs/masq
+EXTENSION=""
+ARCH="$(go env GOHOSTARCH)"
+DISTPATH="bin/linux-${ARCH}"
+if [ "$(go env GOOS)" = "windows" ]; then
+    EXTENSION=".exe"
+    DISTPATH="bin\\windows-${ARCH}"
+fi
+
+go build -o $DISTPATH/masq$EXTENSION github.com/zerklabs/masq
