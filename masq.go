edc0ccc414fa5216ece5f03e741f556a18a28c54
Robin Harper <cabrel@zerklabs.com>
2014-04-16 14:37:59 -0400

    Converted the passwords handler to use a set instead of a zset

--- masq.go
+++ masq.go
@@ -51,6 +51,10 @@ func main() {
 	server.AddRoute("/2/hide", hideHandler)
 	server.AddRoute("/2/show", showHandler)
 	server.AddRoute("/2/passwords", passwordsHandler)
+
+	server.AddRoute("/hide", hideHandler)
+	server.AddRoute("/show", showHandler)
+	server.AddRoute("/passwords", passwordsHandler)
 	server.Start()
 }
 
@@ -143,6 +147,8 @@ func showHandler(req *a.HttpTransaction) {
 func passwordsHandler(req *a.HttpTransaction) {
 	conn, err := redis.Dial("tcp", redisUri)
 
+	// additionalCharacters := []string{"#", "!", "@", "^"}
+
 	if err != nil {
 		req.Error("Failed to connect to redis", 500)
 	}
@@ -152,32 +158,23 @@ func passwordsHandler(req *a.HttpTransaction) {
 	dictionaryKey := fmt.Sprintf("%s:dictionary", *redisKeyPrefix)
 
 	mrand.Seed(time.Now().UTC().UnixNano())
-	r1 := mrand.Intn(80000)
-	r2 := mrand.Intn(80000)
 
-	// get first word of password
-	w1, err := redis.Strings(conn.Do("ZRANGE", dictionaryKey, r1, r1))
+	words, err := redis.Strings(conn.Do("SRANDMEMBER", dictionaryKey, 2))
 
 	if err != nil {
 		req.Error("Failed to retrieve value from Redis", 500)
 	}
 
-	// get second word of password
-	w2, err := redis.Strings(conn.Do("ZRANGE", dictionaryKey, r2, r2))
-
-	if err != nil {
-		req.Error("Failed to retrieve value from Redis", 500)
-	}
-
-	if len(w1) == 0 || len(w2) == 0 {
+	if len(words) == 0 {
 		req.Error("Failed to find value in Redis list", 404)
 	}
 
 	randDigit := mrand.Intn(20000)
+	// randChar := additionalCharacters[mrand.Intn(4)]
 
 	req.RespondWithJSON(struct {
 		Password string `json:"password"`
 	}{
-		Password: fmt.Sprintf("%s%s%d", strings.Title(w1[0]), strings.Title(w2[0]), randDigit),
+		Password: fmt.Sprintf("%s%s%d", strings.Title(words[0]), strings.Title(words[1]), randDigit),
 	})
 }
